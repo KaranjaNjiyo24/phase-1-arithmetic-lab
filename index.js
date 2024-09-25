432537431e35e2ca55b1052f542c7fa90b3854ad
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-09-25 03:22:02 +0300

    arithmetic operations lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// is an equation whose multiplied results will equal 62
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+//generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+//  is an equation that calculates a remainder that is equal to 4
+let num3 = 40
+let num4 = 9
+const mod = (num3 % num4);
+console.log(mod)
+
+//will return 20 as the highest number in the set
+let max = Math.max(12, 5, 20, 17, 8, 18)
+console.log(max)
